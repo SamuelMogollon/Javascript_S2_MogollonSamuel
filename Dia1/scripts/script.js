94c4afc6d1ca81d9bf41c7d8ab7249e6ae631b31
SamuelMogollon <smogollonjanne@gmail.com>
2025-07-18 12:23:39 -0500

    feat :sparkles: into a JavaScript

---
+++ Dia1/scripts/script.js
@@ -0,0 +1,35 @@
+// #######################
+// ###### DIA 1 ##########
+//########################
+
+//¡¡¡Primer comando!!!
+console.log("Hola Mundo!");
+variable1 = 3.14;
+console.log(variable1);
+console.log(typeof variable1);
+//Concatenacion
+palabra1 = "Campus";
+palabra2 = "Lands";
+palabra3 = palabra1 + palabra2;
+console.log(palabra3)
+//Booleanos
+booleanito1=true;
+booleanito2=false;
+console.log(typeof booleanito1);
+//Condicionales
+if(booleanito1 == true){
+    console.log("Es verdadero!!")
+}else{
+    console.log("No es verdadero :sadfeis:")
+}
+//Bucles
+for(i=0; i<4;i=i+1){
+    console.log("Wenas");
+}
+
+holi = true;
+while(holi==true){
+    console.log("Wenass");
+    holi=false;
+}
+// Desarrollado por: Samuel Mogollon - T.I: 1.096.202.198
