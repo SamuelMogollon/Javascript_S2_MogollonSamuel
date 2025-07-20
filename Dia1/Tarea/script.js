984ce9b25062b681f6eeb2b9c0eb8f7528e6f977
SamuelMogollon <smogollonjanne@gmail.com>
2025-07-20 14:49:21 -0500

    feat :sparkles: begin of my homework

---
+++ Dia1/Tarea/script.js
@@ -0,0 +1,18 @@
+booleanito=true;
+while(booleanito==true){
+    console.log(`
+    =============================================
+    Simulador de Gasto Diario
+    =============================================
+    Seleccione una opción:
+
+    1. Registrar nuevo gasto
+    2. Listar gastos
+    3. Calcular total de gastos
+    4. Generar reporte de gastos
+    5. Salir
+    =============================================
+`);
+}
+
+
