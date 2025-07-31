6552c54aa12341e05e8d9c7349907b27ab7e6161
SamuelMogollon <smogollonjanne@gmail.com>
2025-07-31 00:03:22 -0500

    feat :sparkles: homeworkDia9

---
+++ Dia9_Tarea/scripts/script.js
@@ -0,0 +1,19 @@
+function buscarP() {
+    const Id = document.getElementById("mainInput").value;
+    const xhr = new XMLHttpRequest();
+    const url = `https://pokeapi.co/api/v2/pokemon/${Id}`;
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                displayPoke(data)
+            }
+    };
+    xhr.send();
+}
+
+function mostrarP(datos) {
+    const pokemon = document.getElementById("Mostrar");
+    const nombreP = (datos["name"].chatAt(0).toUpperCase()) + (datos["name"].slice(1))
+    pokemon.innerHTML = `${datos["id"]} - ${nombreP}`
+}
