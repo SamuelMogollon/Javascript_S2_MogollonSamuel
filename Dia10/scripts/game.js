9f35edd4bcb038e6f009abef7f0c729055673935
SamuelMogollon <smogollonjanne@gmail.com>
2025-08-02 18:45:46 -0500

    feat :sparkles: udpates

--- Dia10/scripts/game.js
+++ Dia10/scripts/game.js
@@ -1,9 +1,15 @@
 function baraja() {
-    const bajaraNueva = document.getElementById("newBaraja").Value;
+
     const xhr = new XMLHttpRequest();
-    const url = `https://deckofcardsapi.com/api/deck/new/`
+    const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`
     xhr.open("GET", url, true);
-    xhr
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const daticos = JSON.parse(xhr.responseText);
+            console.log(daticos)
+        }
+    };
+    xhr.send();
 }
 
 baraja()
