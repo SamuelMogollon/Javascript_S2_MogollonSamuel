5e51fdc912f154ba7f733048ef1a75c53f6cff29
SamuelMogollon <smogollonjanne@gmail.com>
2025-08-03 16:03:47 -0500

    feat :sparkles: udpates

--- Dia10/scripts/game.js
+++ Dia10/scripts/game.js
@@ -10,10 +10,8 @@ const containerCardsCrupier = document.getElementById("containerCardsCrupier");
 const pedirCartaBtn = document.getElementById("pedirCarta");
 const plantarseBtn = document.getElementById("plantarse");
 
-pedirCartaBtn.addEventListener("click",(e)=>{
-
-});
-plantarseBtn.addEventListener("click");
+pedirCartaBtn.addEventListener('click', () => pedirCarta(true));
+plantarseBtn.addEventListener('click', plantarse);
 
 function hacerPeticion(url) {
   const xhr = new XMLHttpRequest();
@@ -29,4 +27,25 @@ function hacerPeticion(url) {
 }
 hacerPeticion(url)
 
-function 
+async function iniciarJuego(url) {
+  try {
+     const data = await hacerPeticion(`${API_BASE_URL}new/shuffle/?deck_count=6`);
+     deckId = data.deck_id;
+
+    containerCardsPlayer.innerHTML = '';
+    containerCardsCrupier.innerHTML = '';
+    puntuacionJugador = 0;
+    puntuacionCrupier = 0;
+    cartasJugador = [];
+    cartasCrupier = [];
+    pedirCartaBtn.disabled = false;
+    plantarseBtn.disabled = false;
+
+    console.log('Juego Iniciado. Deck ID: ', deckId);
+
+    repartirCartasIniciales();
+  }
+  catch (error) {
+    console.error('Error al iniciar el juego:', error);
+  }
+};
