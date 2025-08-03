f344914281b2013813fcdb2f7743d8aea90fe656
SamuelMogollon <smogollonjanne@gmail.com>
2025-08-03 13:00:13 -0500

    feat :sparkles: udpates

--- Dia10/scripts/game.js
+++ Dia10/scripts/game.js
@@ -1,15 +1,32 @@
-function baraja() {
+const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+let deckId = "";
+let cartasJugador = [];
+let cartasCrupier = [];
+let puntuacionJugador = 0;
+let puntuacionCrupier = 0;
 
-    const xhr = new XMLHttpRequest();
-    const url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6`
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const daticos = JSON.parse(xhr.responseText);
-            console.log(daticos)
-        }
-    };
+const containerCardsPlayer = document.getElementById("containerCardsPlayer");
+const containerCardsCrupier = document.getElementById("containerCardsCrupier");
+const pedirCartaBtn = document.getElementById("pedirCarta");
+const plantarseBtn = document.getElementById("plantarse");
+
+pedirCartaBtn.addEventListener("click",(e)=>{
+
+});
+plantarseBtn.addEventListener("click");
+
+function hacerPeticion(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      console.log(data);
+      
+    }
     xhr.send();
+  };
 }
+hacerPeticion(url)
 
-baraja()
+function 
