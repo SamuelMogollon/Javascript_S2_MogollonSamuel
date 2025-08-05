b389c1dbd72847708168af204da6c796ef4dea8a
SamuelMogollon <smogollonjanne@gmail.com>
2025-08-05 11:29:55 -0500

    feat :sparkles: udpates

--- Dia9_Tarea/scripts/script.js
+++ Dia9_Tarea/scripts/script.js
@@ -1,19 +1,27 @@
-+function buscarP() {
-    const Id = document.getElementById("mainInput").value;
+function mirarData(event){
+    if (event.key === 'Enter'){
+        buscarP();
+    }
+}
+
+
+function buscarP() {
+    document.getElementById("mostrar").innerHTML``;
+    const Id = document.getElementById("textbox").value;
     const xhr = new XMLHttpRequest();
-    const url = `https://pokeapi.co/api/v2/pokedex/${Id}`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${Id}`;
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
                 const data = JSON.parse(xhr.responseText);
-                displayPoke(data)
+                let mostrarData = document.getElementById("mostrar")
+                mostrarData.innerHTML = `
+                <h1 class="pokemon"> ${data["id"]}. </h1>
+                <h1 class="nombreP">${data["name"]} </h1>
+                <img src="${data["sprites"]["other"]["showdown"]["front_default"]}">
+                `
             }
     };
     xhr.send();
 }
 
-function mostrarP(datos) {
-    const pokemon = document.getElementById("Mostrar");
-    const nombreP = (datos["name"].chatAt(0).toUpperCase()) + (datos["name"].slice(1))
-    pokemon.innerHTML = `${datos["id"]} - ${nombreP}`
-}
