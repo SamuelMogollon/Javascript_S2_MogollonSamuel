a6ccb667c07e41c8d326da732d5afa7b0b63f42e
SamuelMogollon <smogollonjanne@gmail.com>
2025-08-05 22:54:22 -0500

    feat :sparkles: udpates

--- Dia12+1/scripts/script.js
+++ Dia12+1/scripts/script.js
@@ -1,36 +1,36 @@
 function buscarPersonaje() {
-    document.getElementById("resultados").innerHTML = ``;
-    const nombreUsar = document.getElementById("nombreInput").value.trim();
-    console.log(nombreUsar);
-    const xhr = new XMLHttpRequest();
-    const url = `https://superheroapi.com/api.php/797624f341008e7444b52ecdb74a1481/search/${nombreUsar}`;
-    console.log(url);
-    xhr.open("GET", url, true);
+  document.getElementById("resultados").innerHTML = ``;
+  const nombreUsar = document.getElementById("nombreInput").value.trim();
+  console.log(nombreUsar);
+  const xhr = new XMLHttpRequest();
+  const url = `https://superheroapi.com/api.php/797624f341008e7444b52ecdb74a1481/search/${nombreUsar}`;
+  console.log(url);
+  xhr.open("GET", url, true);
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 3) {
-            
-            console.log ("cargando...")
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 3) {
+      console.log("cargando...");
+    } else if (xhr.readyState === 4 && xhr.status === 200) {
+      try {
+        const daticos = JSON.parse(xhr.responseText);
+        if (daticos.results && daticos.results.length > 0) {
+          for (let i = 0; i < daticos.results.length; i++) {
+            let resultado = document.getElementById("resultados");
+            resultado.innerHTML += `
+                        <div class= "d-flex justify-content-center col-2 ">
+                            <div class="card">
+                                <img class=""src="${daticos["results"][i]["image"]["url"]}" class="card-img-top">
+                                <div class="card-body">
+                                <p class="card-text">${daticos["results"][i]["name"]}</p>
+                                </div>
+                            </div>
+                        </div>`;
+          }
         }
-        else if (xhr.readyState === 4 && xhr.status === 200) {
-            try {
-                const daticos = JSON.parse(xhr.responseText);
-                if (daticos.results && daticos.results.length > 0) {
-                    for (let i =0; i < daticos.results.length; i++) {
-                        let resultado = document.getElementById("resultados");
-                        resultado.innerHTML += `
-                        <div class="card">
-                        <img src="${daticos["results"][i]["image"]["url"]}">
-                        <h3>${daticos["results"][i]["name"]}</h3>
-                        </div>`
-                    }
-                }    
-            }
-            catch (err) {
-                console.log (err.message);
-            }
-        }
+      } catch (err) {
+        console.log(err.message);
+      }
     }
-    xhr.send();
+  };
+  xhr.send();
 }
-
