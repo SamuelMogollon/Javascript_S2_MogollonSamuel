3485e1b68cb067edd7e6753445cbf12a6c04b341
SamuelMogollon <smogollonjanne@gmail.com>
2025-08-08 13:26:35 -0500

    feat :sparkles: udpates

--- Dia9_Tarea/scripts/script.js
+++ Dia9_Tarea/scripts/script.js
@@ -1,27 +1,35 @@
-function mirarData(event){
-    if (event.key === 'Enter'){
-        buscarP();
-    }
-}
-
-
-function buscarP() {
-    document.getElementById("mostrar").innerHTML``;
-    const Id = document.getElementById("textbox").value;
-    const xhr = new XMLHttpRequest();
-    const url = `https://pokeapi.co/api/v2/pokemon/${Id}`;
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-                const data = JSON.parse(xhr.responseText);
-                let mostrarData = document.getElementById("mostrar")
-                mostrarData.innerHTML = `
-                <h1 class="pokemon"> ${data["id"]}. </h1>
-                <h1 class="nombreP">${data["name"]} </h1>
-                <img src="${data["sprites"]["other"]["showdown"]["front_default"]}">
-                `
+//creo una funcion para buscar el pokemom
+function buscarP(nombrePokemon) {
+    //creo una instancia de la clase xmlhttpRequest
+    const xml = new XMLHttpRequest()
+    //declaro una variable para guardar el endpoint
+    const url = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`
+    //creo la peticion al endpoint
+    xml.open("GET", url, true);
+    // el metodo que seva a ejecutar cada vez que la peticion cambie de estado
+    xml.onreadystatechange = function () {
+        //creo una condicional que me diga si se esta ejecutando la funcion
+        if (xml.readyState === 3) {
+            console.log("cargando...");
+            // cxomparo la solicitud completa para comprobar si es exitosa
+        } else if (xml.readyState === 4 && xml.status === 200) {
+            try {
+                //guarda la respuesta de la peticion en la variable "respuesta"
+                let respuesta = JSON.parse(xml.responseText)
+                document.getElementById("")
+            } catch (err) {
+                console.log(err.message);
             }
+        }
     };
-    xhr.send();
+    xml.send();
 }
 
+
+
+let nombrePokemon = document.getElementById("textbox");
+nombrePokemon.addEventListener("keyup", function (presionarEnter) {
+    if (presionarEnter.key === "Enter") {
+        buscarP(nombrePokemon.value)
+    }
+})
